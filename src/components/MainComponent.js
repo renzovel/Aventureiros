3798de91a8ad9245d74fc72f4775ce5280cb1cac
eadrianc <102111847+eadrianc@users.noreply.github.com>
2022-07-27 02:29:05 -0300

    footer pronto

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,65 @@ import "../assets/styles.css";
 function MainComponent() {
 return (
 <main className="main-component">
-<h3>main em trabajada</h3>
+<div className="container">
+<div className="item">
+<a href="https://th.bing.com/th/id/R.d19c81f9896d21586ddda103939aa6ab?rik=eHpnz5pzoCZN7w&riu=http%3a%2f%2f4.bp.blogspot.com%2f-JMRxbToBEbo%2fUN1loPjkGQI%2fAAAAAAAAsGo%2fIX262KitL70%2fs1600%2fAngel-Falls-Venezuela.jpg&ehk=TUWJ1gXC%2bMkfRiGB23GoxM0XH%2fUtdK7yyl8wW6iC1Ao%3d&risl=&pid=ImgRaw&r=0">
+<img src="https://lonelyplanetimages.imgix.net/a/g/hi/t/0381f510b6548084b87b1726aa8a5a6b-salto-angel-angel-falls.jpg" alt="salto anguel" className="card__imagem" />
+<div className="card__info">
+<span className="card__badge">x</span>
+<span className="card__precoanterior">x</span>
+</div>
+</a>
+</div>
+<div className="item">
+<a href="https://th.bing.com/th/id/OIP.uwosIcXHWzwHI81HzatG7wHaFI?pid=ImgDet&rs=1">
+<img src="https://th.bing.com/th/id/OIP.uwosIcXHWzwHI81HzatG7wHaFI?pid=ImgDet&rs=1" alt="salto anguel" className="card__imagem" />
+<div className="card__info">
+<span className="card__badge">x</span>
+<span className="card__precoanterior">x</span>
+</div>
+</a>
+</div>
+<div className="item">
+<a href="https://th.bing.com/th/id/R.d19c81f9896d21586ddda103939aa6ab?rik=eHpnz5pzoCZN7w&riu=http%3a%2f%2f4.bp.blogspot.com%2f-JMRxbToBEbo%2fUN1loPjkGQI%2fAAAAAAAAsGo%2fIX262KitL70%2fs1600%2fAngel-Falls-Venezuela.jpg&ehk=TUWJ1gXC%2bMkfRiGB23GoxM0XH%2fUtdK7yyl8wW6iC1Ao%3d&risl=&pid=ImgRaw&r=0">
+<img src="https://lonelyplanetimages.imgix.net/a/g/hi/t/0381f510b6548084b87b1726aa8a5a6b-salto-angel-angel-falls.jpg" alt="salto anguel" className="card__imagem" />
+<div className="card__info">
+<span className="card__badge">x</span>
+<span className="card__precoanterior">x</span>
+</div>
+</a>
+</div>
+</div>
+
+<div className="container">
+<div className="item">
+<a href="https://th.bing.com/th/id/R.d19c81f9896d21586ddda103939aa6ab?rik=eHpnz5pzoCZN7w&riu=http%3a%2f%2f4.bp.blogspot.com%2f-JMRxbToBEbo%2fUN1loPjkGQI%2fAAAAAAAAsGo%2fIX262KitL70%2fs1600%2fAngel-Falls-Venezuela.jpg&ehk=TUWJ1gXC%2bMkfRiGB23GoxM0XH%2fUtdK7yyl8wW6iC1Ao%3d&risl=&pid=ImgRaw&r=0">
+<img src="https://lonelyplanetimages.imgix.net/a/g/hi/t/0381f510b6548084b87b1726aa8a5a6b-salto-angel-angel-falls.jpg" alt="salto anguel" className="card__imagem" />
+<div className="card__info">
+<span className="card__badge">x</span>
+<span className="card__precoanterior">x</span>
+</div>
+</a>
+</div>
+<div className="item">
+<a href="https://th.bing.com/th/id/R.d19c81f9896d21586ddda103939aa6ab?rik=eHpnz5pzoCZN7w&riu=http%3a%2f%2f4.bp.blogspot.com%2f-JMRxbToBEbo%2fUN1loPjkGQI%2fAAAAAAAAsGo%2fIX262KitL70%2fs1600%2fAngel-Falls-Venezuela.jpg&ehk=TUWJ1gXC%2bMkfRiGB23GoxM0XH%2fUtdK7yyl8wW6iC1Ao%3d&risl=&pid=ImgRaw&r=0">
+<img src="https://lonelyplanetimages.imgix.net/a/g/hi/t/0381f510b6548084b87b1726aa8a5a6b-salto-angel-angel-falls.jpg" alt="salto anguel" className="card__imagem" />
+<div className="card__info">
+<span className="card__badge">x</span>
+<span className="card__precoanterior">x</span>
+</div>
+</a>
+</div>
+<div className="item">
+<a href="https://th.bing.com/th/id/R.d19c81f9896d21586ddda103939aa6ab?rik=eHpnz5pzoCZN7w&riu=http%3a%2f%2f4.bp.blogspot.com%2f-JMRxbToBEbo%2fUN1loPjkGQI%2fAAAAAAAAsGo%2fIX262KitL70%2fs1600%2fAngel-Falls-Venezuela.jpg&ehk=TUWJ1gXC%2bMkfRiGB23GoxM0XH%2fUtdK7yyl8wW6iC1Ao%3d&risl=&pid=ImgRaw&r=0">
+<img src="https://lonelyplanetimages.imgix.net/a/g/hi/t/0381f510b6548084b87b1726aa8a5a6b-salto-angel-angel-falls.jpg" alt="salto anguel" className="card__imagem" />
+<div className="card__info">
+<span className="card__badge">x</span>
+<span className="card__precoanterior">x</span>
+</div>
+</a>
+</div>
+</div>
 </main>
 );
 }
