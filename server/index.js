11d29f0689e77b0c28118f68f690ed3076af79fb
Renzo Veliz <renzo.veliz1br@gmail.com>
2022-07-30 10:44:06 -0300

    parseando as requisicoes e os response a json

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ var PORT = 3001;
 var trilhas = require('./router/trilhas');
 var usuarios = require('./router/usuarios');
 
-app.use(cors())
+app.use(cors());
+
+app.use(express.json());
 
 app.use('/api/trilhas', trilhas);
 
